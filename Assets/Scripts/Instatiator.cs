ce798db4765cdaf72004ee19bea1233343d7f434
LucianRoitman <49261770@est.ort.edu.ar>
2025-05-12 10:23:19 -0300

    clone cube

---
+++ Assets/Scripts/Instatiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instatiator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ClonarCubo();
+        
+    }
+
+    public void ClonarCubo()
+    {
+        Instantiate(cubePrefab);
+    }
+}
