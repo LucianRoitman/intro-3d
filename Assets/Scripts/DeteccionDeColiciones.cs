12009c5ad52484661f13b91c4745fbeda8e6846c
LucianRoitman <49261770@est.ort.edu.ar>
2025-05-22 10:26:39 -0300

    collsion

---
+++ Assets/Scripts/DeteccionDeColiciones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColiciones : MonoBehaviour
+{
+  void OnCollisionEnter()
+    {
+        Debug.Log("acto acto pide contacto quiere que lo hagamos cada rato, duro dos horas haciendolo bien rico o de las chicas soy su favorito o");
+        Destroy(gameObject);
+    }
+}
