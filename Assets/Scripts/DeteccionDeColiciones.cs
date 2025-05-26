0d13af0d247477b6f700d0e9996b1ba44c77e2eb
LucianRoitman <49261770@est.ort.edu.ar>
2025-05-26 10:40:28 -0300

    UI,ScorePoints

--- Assets/Scripts/DeteccionDeColiciones.cs
+++ Assets/Scripts/DeteccionDeColiciones.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class DeteccionDeColiciones : MonoBehaviour
 {
-  void OnCollisionEnter(Collision col)
+    public ScoreManager ScoreManager;
+    public MercaderiaScript mercaderiaScript;
+
+     void Start()
+    {
+        ScoreManager = FindObjectOfType<ScoreManager>();
+        mercaderiaScript = GetComponent<MercaderiaScript>();
+    }
+
+    void OnCollisionEnter(Collision col)
     {
         Debug.Log("contacto");
         if (col.gameObject.name == "Player")
         {
             Destroy(gameObject);
+            ScoreManager.addScore(mercaderiaScript.scorePoints);
         }
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public UiManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = FindObjectOfType<UiManager>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    public void addScore()
+    {
+        score++;
+        uiManager.UpdateScore(score);
+    }
+    public void addScore(int scorePoints)
+    {
+        score += scorePoints;
+        uiManager.UpdateScore(score);
+    }
+}
