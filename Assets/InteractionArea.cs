2bf0d96068687124ffe2f8b46fd006c86bb31c99
LucianRoitman <49261770@est.ort.edu.ar>
2025-07-10 10:39:59 -0300

    terminar los muelles

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject UIInteractionMessage;
+    public bool canInteract;
+    public InteractionObject mercaderia;
+    public ScoreManager scoreManager;
+    public int puntos;
+
+    void Start()
+    {
+        UIInteractionMessage.SetActive(false);
+        scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (canInteract)
+            {
+                Destroy(mercaderia.gameObject);
+                scoreManager.addScore(puntos);
+                EndInteraction();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log(other.gameObject.name);
+        mercaderia = other.GetComponent<InteractionObject>();
+        if (mercaderia)
+        {
+            UIInteractionMessage.SetActive(true);
+            canInteract = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        EndInteraction();
+    }
+
+    void EndInteraction()
+    {
+        UIInteractionMessage.SetActive(false);
+        mercaderia = null;
+        canInteract = false;
+    }
+}
